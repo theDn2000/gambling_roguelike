14d63fa62704144234a43946bf3f61e22d54cd5b
Daniel <theDn2000@gmail.com>
2025-01-23 13:08:08 +0100

    Test_2

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+// This is a test file for the gambling_roguelike project.
+
+#include <iostream>
+#include <ostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+    return 0;
+}
